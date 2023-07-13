400ef6248290227ca535e76a05b800e42650b000
Jonathan Adly <gadly0123@gmail.com>
2023-07-13 12:06:35 -0400

    static files

--- config/settings.py
+++ config/settings.py
@@ -102,3 +102,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
