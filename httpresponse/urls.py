7ceecb0bbad9398626834e30502ce5c50e836f2e
Jonathan Adly <gadly0123@gmail.com>
2023-07-14 00:22:09 -0400

    complete

--- httpresponse/urls.py
+++ httpresponse/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("documentation", views.documentation, name="documentation"),
     path("tag/<str:tag_name>", views.tag_name, name="tag_name"),
+    path("escape/<str:tag_name>", views.tag_name_escaped, name="tag_name_escaped"),
+    path("gist/<str:gist_id>", views.github_gist, name="gist"),
 ]
--- httpresponse/views.py
+++ httpresponse/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import HTMLTag
+from html import escape
 
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
 from django.views.decorators.csrf import csrf_exempt
+
+from .models import HTMLTag
 
 
 def home(request):
-    return render(request, "home.html")
+    table_code = HTMLTag.objects.get(html_tag="table").content
+    url = request.build_absolute_uri("")
+    code = (
+        f"<button hx-get='{url}tag/table?class=table%20table-bordered'> Get </button>"
+    )
+    gist = f"<button hx-get='{url}gist/c9fd72b8f8a265d8bfcdb4338ffc76fa'> Get Gist </button>"
+    tags = HTMLTag.objects.all()
+    return render(
+        request,
+        "home.html",
+        {"table_code": table_code, "code": code, "tags": tags, "gist": gist},
+    )
+
+
+def documentation(request):
+    # get root url
+    url = request.build_absolute_uri(reverse("home"))
+    return render(request, "documentation.html", {"url": url})
 
 
 @csrf_exempt
@@ -17,19 +37,78 @@ def tag_name(request, tag_name):
         return HttpResponse(
             "We don't have a html tag called "
             + tag_name
-            + ". Please try again with a common html ."
+            + ". Please try again with a common html tag."
         )
     if request.method == "GET":
-        return HttpResponse(fragment.content)
+        classes = request.GET.get("class", "")
+        content = fragment.content
+        if classes:
+            content = content.replace(">", f' class="{classes}">', 1)
+        return HttpResponse(content)
     elif request.method == "POST":
         values = request.POST.values()
         return HttpResponse(
             fragment.content + "You Posted the following values: " + " ".join(values)
         )
     elif request.method == "PUT":
+        return HttpResponse("Put Request Received")
+    elif request.method == "DELETE":
+        return HttpResponse("Delete Request Received")
+
+
+@csrf_exempt
+def tag_name_escaped(request):
+    # this to demonstrate in the documentation siste
+    try:
+        fragment = HTMLTag.objects.get(html_tag=tag_name.lower())
+    except HTMLTag.DoesNotExist:
+        return HttpResponse(
+            escape(
+                "We don't have a html tag called "
+                + tag_name
+                + ". Please try again with a common html tag."
+            )
+        )
+    if request.method == "GET":
+        classes = request.GET.get("class", "")
+        content = fragment.content
+        if classes:
+            content = content.replace(">", f' class="{classes}">', 1)
+        return HttpResponse(escape(content))
+    elif request.method == "POST":
+        values = request.POST.values()
+        return HttpResponse(
+            escape(
+                fragment.content
+                + "<br> You Posted the following values: "
+                + " ".join(values)
+            )
+        )
+    elif request.method == "PUT":
         values = request.PUT.values()
         return HttpResponse(
-            fragment.content + "You Put the following values: " + " ".join(values)
+            escape(
+                fragment.content
+                + "<br> You Put the following values: "
+                + " ".join(values)
+            )
         )
     elif request.method == "DELETE":
         return HttpResponse("Delete Request Received")
+
+
+def github_gist(request, gist_id):
+    api_url = f"https://api.github.com/gists/{gist_id}"
+    response = requests.get(api_url)
+    gist_data = response.json()
+    html_content = None
+    if "files" in gist_data:
+        files = gist_data["files"]
+        for filename, file_data in files.items():
+            if file_data["language"] == "HTML":
+                html_content = file_data["content"]
+                return HttpResponse(html_content)
+
+    return HttpResponse(
+        "No HTML file found in this gist. Please make sure your gist is public, and contains an HTML file. See example here: https://gist.github.com/Jonathan-Adly/c9fd72b8f8a265d8bfcdb4338ffc76fa"
+    )
